add693f725efd3c7881de6213d659801ed788faf
UCS\e.olt <e.olt@ucs.ru>
2018-12-25 18:57:43 +0300

    добавил индекс и Amount

--- sql_scripts/Rates.sql
+++ sql_scripts/Rates.sql
@@ -1,11 +1,14 @@
---DROP TABLE public."Rates";
+DROP TABLE public."Rates";
 
 CREATE TABLE public."Rates" (
 	"Id" uuid NOT NULL,
 	"Code" text NOT NULL,
 	"Value" numeric(18, 2) NOT null,
+	"Amount" int NOT null,
 	"Date" date NOT NULL
 )
 WITH (
 	OIDS=FALSE
 ) ;
+
+CREATE UNIQUE INDEX date_code_idx ON public."Rates" ("Date", "Code");
